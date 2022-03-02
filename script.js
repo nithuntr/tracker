ede0b48b76fcb35a8bddaecb9f5054a64f38afd2
nithuntr <nithuntr19@gmail.com>
2022-03-02 13:22:39 +0530

    tracker

---
+++ script.js
@@ -0,0 +1,27 @@
+let income=0;
+let expense=0;
+let balance;
+function addStorage()
+{   
+    var text=document.getElementById("text").value;
+    var amount=document.getElementById("amount").value
+    localStorage.setItem(text,amount);
+    //console.log(JSON.parse(localStorage.getItem(text,amount)));
+    var display = document.createElement('div');
+    display.setAttribute("id","list1");
+    console.log(display);
+    document.getElementById('list').appendChild(display).innerHTML=text+"........."+amount;
+   if((Math.sign(amount))==1)
+     {
+        income=Number(income)+Number(amount);
+        document.getElementById('money-plus').textContent="+$"+income;
+    }
+    else
+     {
+            expense=Number(expense)+Number(amount);
+            document.getElementById('money-minus').textContent="$"+expense;
+     }
+     //document.getElementById('list').innerHTML=text+"........."+amount;
+     balance=Number(income)+Number(expense);    
+     document.getElementById('balance').textContent="$"+balance;
+}
